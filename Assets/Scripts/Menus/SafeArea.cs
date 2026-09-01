2b9ffcd553fc6d9087439625d0ab156527174eb1
FranciscoPasini <franciscopasini8@gmail.com>
2026-08-31 21:34:48 -0300

    SafeArea2

---
+++ Assets/Scripts/Menus/SafeArea.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class SafeArea : MonoBehaviour
+{
+    RectTransform panel;
+    Rect lastSafeArea = new Rect(0, 0, 0, 0);
+
+    void Awake()
+    {
+        panel = GetComponent<RectTransform>();
+        ApplySafeArea();
+    }
+
+    void Update()
+    {
+        if (Screen.safeArea != lastSafeArea)
+            ApplySafeArea();
+    }
+
+    void ApplySafeArea()
+    {
+        Rect safeArea = Screen.safeArea;
+        lastSafeArea = safeArea;
+
+        Vector2 anchorMin = safeArea.position;
+        Vector2 anchorMax = safeArea.position + safeArea.size;
+
+        anchorMin.x /= Screen.width;
+        anchorMin.y /= Screen.height;
+        anchorMax.x /= Screen.width;
+        anchorMax.y /= Screen.height;
+
+        panel.anchorMin = anchorMin;
+        panel.anchorMax = anchorMax;
+    }
+}
